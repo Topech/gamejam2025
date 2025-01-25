b6168487dffbf7269af8ef25eca3fdc34b29a367
ZacUni <42055333+ZacUni@users.noreply.github.com>
2025-01-25 11:01:51 +0800

    Create button.gd

---
+++ button.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta: float) -> void:
+
+
+
+func _on_start_button_pressed() -> void:
+	#get_tree().change_scene()
+	pass
+
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
