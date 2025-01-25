b2d96cab84ab561ebe7c0aa79f1a78d1f567a276
BenEnco <ben.heeley@enco.net.au>
2025-01-25 18:37:06 +0800

    Added Main menu background
    Background added to main menu
    Play button refactored

--- start_button.gd
+++ start_button.gd
@@ -1,29 +1,28 @@
 extends Button
 
-var speed = 0.25
+var speed = 0.025
 var n = 0
-var x = 20
+var dist_threshold = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	
-	if (n >= 0 && n < (1 * x)):
+	if (n >= 0 && n < (1 * dist_threshold)):
 		position += Vector2.RIGHT * speed
 		n += 1
-	elif (n >= (1 * x) && n < (2 * x)):
+	elif (n >= (1 * dist_threshold) && n < (2 * dist_threshold)):
 		position += Vector2.DOWN * speed
 		n += 1
-	elif (n >= (2 * x) && n < (3 * x)):
+	elif (n >= (2 * dist_threshold) && n < (3 * dist_threshold)):
 		position += Vector2.LEFT * speed
 		n += 1
-	elif (n >= (3 * x) && n < (4 * x)):
+	elif (n >= (3 * dist_threshold) && n < (4 * dist_threshold)):
 		position += Vector2.UP * speed
 		n += 1
-	elif n >= 4 * x:
+	elif n >= 4 * dist_threshold:
 		n = 0
 			
