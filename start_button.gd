479364ddf719f607cc0fc03aae04bd176f1240bb
ZacUni <42055333+ZacUni@users.noreply.github.com>
2025-01-25 15:52:27 +0800

    START BUTTON MOVES

--- start_button.gd
+++ start_button.gd
@@ -1,5 +1,8 @@
 extends Button
 
+var speed = 0.25
+var n = 0
+var x = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +10,20 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(_delta: float) -> void:
+	
+	if (n >= 0 && n < (1 * x)):
+		position += Vector2.RIGHT * speed
+		n += 1
+	elif (n >= (1 * x) && n < (2 * x)):
+		position += Vector2.DOWN * speed
+		n += 1
+	elif (n >= (2 * x) && n < (3 * x)):
+		position += Vector2.LEFT * speed
+		n += 1
+	elif (n >= (3 * x) && n < (4 * x)):
+		position += Vector2.UP * speed
+		n += 1
+	elif n >= 4 * x:
+		n = 0
+			
