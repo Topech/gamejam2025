d39927518ffbf8b7f2aeaa70fccc58878d1a63d7
Topech <benlbttm0@gmail.com>
2025-01-25 19:53:27 +0800

    fix going to upgrade screen

--- round_scoreboard.gd
+++ round_scoreboard.gd
@@ -21,5 +21,4 @@ func calc_money(metrics):
 
 
 func _on_next_button_pressed() -> void:
-	# get_tree().change_scene_to_file("res://upgrades.tscn")
-	pass
+	get_tree().change_scene_to_file("res://upgrade_screen.tscn")
