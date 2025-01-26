d9419c3b5842fd11fa65cb9e48593193f6ceb596
Curtis Nuich <c.nuich@gmail.com>
2025-01-26 14:57:20 +0800

    pump sfx

--- global.gd
+++ global.gd
@@ -8,27 +8,29 @@ extends Node
 
 var button_player: AudioStreamPlayer
 var victory_player: AudioStreamPlayer
+var pop_player: AudioStreamPlayer
+var pump_player: AudioStreamPlayer
+
+# for pump
+var interval_timer = 0.7
+var min_pitch = 0.7
+var max_pitch = 1.3
+var rng = RandomNumberGenerator.new()
+var sxf_timer: Timer
+var is_pump_playing: bool = false  
 
 
 func add_money(amount: int) -> void:
 	player_money += amount
 
-
-
 func add_soap() -> void:
 	soap_level += 1
-
-
 
 func add_water(added_water: String) -> void:
 	unlocked_water.append(added_water)
 
-
-
 func reset_money() -> void:
 	player_money = 0
-
-
 
 func spend_money(amount: int) -> bool:
 	if player_money >= amount:
@@ -36,14 +38,11 @@ func spend_money(amount: int) -> bool:
 		return true
 	return false
 
-
-
 func get_money() -> int:
 	return player_money
-	
+
 func get_soap() -> int:
 	return soap_level
-
 
 func play_button_sound():
 	button_player.play()
@@ -51,12 +50,44 @@ func play_button_sound():
 func play_victory_sound():
 	victory_player.play()
 
+func play_pop_sound():
+	pop_player.play()
+
+func play_pump_sound():
+	if is_pump_playing:
+		pump_player.pitch_scale = randf_range(min_pitch, max_pitch)
+		pump_player.play()
+
+func start_pump():
+	is_pump_playing = true
+	play_pump_sound()
+	sxf_timer.start()
+
+func stop_pump():
+	is_pump_playing = false
+	sxf_timer.stop()
+
 func _ready() -> void:
 	button_player = AudioStreamPlayer.new()
 	add_child(button_player)
-	button_player.stream = preload("res://bubble_tiny.wav")
+	button_player.stream = preload("res://sfx_bubble_tiny.wav")
 	
 	victory_player = AudioStreamPlayer.new()
 	add_child(victory_player)
-	victory_player.stream = preload("res://victory.ogg")
+	victory_player.stream = preload("res://sfx_victory.ogg")
 	
+	pop_player = AudioStreamPlayer.new()
+	add_child(pop_player)
+	pop_player.stream = preload("res://sfx_pop.wav")
+	interval_timer = Timer.new()
+	add_child(interval_timer)
+	
+	#sfx pump
+	pump_player = AudioStreamPlayer.new()
+	add_child(pump_player)
+	pump_player.stream = preload("res://sfx_pump.wav")
+	sxf_timer = Timer.new()
+	add_child(sxf_timer)
+	sxf_timer.wait_time = 0.5
+	sxf_timer.one_shot = false
+	sxf_timer.connect("timeout", play_pump_sound)
--- round.gd
+++ round.gd
@@ -63,10 +63,12 @@ func _process(delta: float) -> void:
 
 
 func pop():
+	Global.stop_pump()
 	$StopButton.disabled = true
 	$Bubble/BubblePopSprite.play_pop()
 	is_growing = false
 	emit_signal("popped")
+	Global.play_pop_sound()
 	BgmPlayer.play_song_game_over()
 	await get_tree().create_timer(0.6).timeout
 	get_tree().change_scene_to_file("res://gameover.tscn")
@@ -79,11 +81,13 @@ func _on_blow_button_pressed() -> void:
 	$StopButton.disabled = false
 	$BlowButton.visible = false
 	$StopButton.visible = true
+	Global.start_pump()
 
 
 
 func _on_stop_button_pressed() -> void:
 	is_growing = false
+	Global.stop_pump()
 	$StopButton.disabled = true
 	var metrics = MetricStruct.new(growth_time_elapsed, all_growth)
 	Global.round_metrics = metrics
